73fbf83149d31a8f4e1103ab21142a02ad773e53
eioforkansi <eioforkansi@gmail.com>
2024-10-27 06:24:59 +0100

    Task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,37 +1,38 @@
 #!/usr/bin/env python3
-
-"""
-Module with the implementation of get_page which
-uses the request module to obtain the HTML content
-of a particular URL and returns it.  
-"""
-
-import requests
+'''A module with tools for request caching and tracking.
+'''
 import redis
-from typing import Callable
+import requests
 from functools import wraps
+from typing import Callable
 
-cache = redis.Redis()
 
-def count(func: Callable) -> Callable:
-    @wraps(func)
-    def wrapper(url, *args, **kwds):
-        cache.incr(f"count:{url}")
-        result = func(url, *args, **kwds)
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
         return result
-    return wrapper
+    return invoker
 
-@count
+
+@data_cacher
 def get_page(url: str) -> str:
-    content = cache.get(url)
-
-    if content:
-        return content.decode("utf-8")
-    else:
-        response = requests.get(url)
-        cache.setex(url, 10, response.text)
-        return response.text
-
-url = "http://slowwly.robertomurray.co.uk"
-get_page(url)
-print(cache.get(f"count:{url}"))
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
